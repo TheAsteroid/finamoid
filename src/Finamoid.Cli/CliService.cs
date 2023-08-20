0d9ba3989295eb64dbbc41f5b3e3438f8c98c735
TheAsteroid <41196601+TheAsteroid@users.noreply.github.com>
2023-08-20 17:49:42 +0200

    CLI scaffold #5

---
+++ src/Finamoid.Cli/CliService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Hosting;
+using Spectre.Console;
+
+namespace Finamoid.Cli
+{
+    internal class CliService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)            
+            {
+                stoppingToken.ThrowIfCancellationRequested();
+
+                var funcs = new Dictionary<string, Func<CancellationToken, Task>>
+                {
+                    { "import", ImportAsync },
+                    { "categorize", CategorizeAsync },
+                    { "aggregate", AggregateAsync },
+                    { "exit", ExitAsync }
+                };
+
+                var selectionPrompt = new SelectionPrompt<string>
+                {
+                    Title = "What do you want to do?"
+                };
+                selectionPrompt.AddChoices(funcs.Keys);
+
+                var action = AnsiConsole.Prompt(selectionPrompt);
+
+                AnsiConsole.Clear();
+                AnsiConsole.WriteLine($"Ok, let's {action} stuff!");
+
+                await funcs[action](stoppingToken);
+            }
+        }
+
+        private Task ImportAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task CategorizeAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task AggregateAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task ExitAsync(CancellationToken stoppingToken)
+        {
+            AnsiConsole.WriteLine("Goodbye...");
+
+            Environment.Exit(0);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Finamoid.Cli/Program.cs
@@ -0,0 +1,17 @@
+﻿using Finamoid.Cli;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateDefaultBuilder();
+
+builder.ConfigureServices(s =>
+{
+    s.AddHostedService<CliService>();
+});
+
+builder.ConfigureLogging(l => l.SetMinimumLevel(LogLevel.Warning));
+
+using var host = builder.Build();
+
+await host.RunAsync();
