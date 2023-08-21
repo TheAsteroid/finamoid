1788c8c4f13f58736773b3a47677d7a758fa04c5
TheAsteroid <41196601+TheAsteroid@users.noreply.github.com>
2023-08-21 17:58:02 +0200

    Add more date validation #5

--- src/Finamoid.Utils/FileHelper.cs
+++ src/Finamoid.Utils/FileHelper.cs
@@ -7,19 +7,33 @@ namespace Finamoid.Utils
         private const string _weekPrefix = "w-";
         private const string _monthPrefix = "m-";
 
-        public static string GetFileNameDatePart(DateTime dateTime, PeriodType periodType) => periodType switch
+        public static string GetFileNameDatePart(DateTime dateTime, PeriodType periodType)
         {
-            PeriodType.Week => Path.Combine(ISOWeek.GetYear(dateTime).ToString(), $"{_weekPrefix}{ISOWeek.GetWeekOfYear(dateTime):00}"),
-            PeriodType.Month => Path.Combine(dateTime.Year.ToString(), $"{_monthPrefix}{dateTime.Month:00}"),
-            PeriodType.Year => dateTime.Year.ToString(),
-            _ => throw new InvalidOperationException($"{nameof(PeriodType)} must be defined.")
-        };
+            if (dateTime.Kind != DateTimeKind.Utc)
+            {
+                throw new NotSupportedException($"{nameof(GetFileNameDatePart)} only supports UTC dates.");
+            }
+
+            return periodType switch
+            {
+                PeriodType.Week => Path.Combine(ISOWeek.GetYear(dateTime).ToString(), $"{_weekPrefix}{ISOWeek.GetWeekOfYear(dateTime):00}"),
+                PeriodType.Month => Path.Combine(dateTime.Year.ToString(), $"{_monthPrefix}{dateTime.Month:00}"),
+                PeriodType.Year => dateTime.Year.ToString(),
+                _ => throw new InvalidOperationException($"{nameof(PeriodType)} must be defined.")
+            };
+        }
 
         public static bool FileNameIsInPeriod(string fileName, DateTime? startDate, DateTime? endDate)
         {
             if (startDate == null && endDate == null)
             {
                 return true;
+            }
+
+            if (startDate.HasValue && startDate?.Kind != DateTimeKind.Utc || 
+                endDate.HasValue && endDate?.Kind != DateTimeKind.Utc)
+            {
+                throw new NotSupportedException($"{nameof(FileNameIsInPeriod)} only supports UTC dates.");
             }
 
             DateTime minDate = startDate ?? DateTime.MinValue;
